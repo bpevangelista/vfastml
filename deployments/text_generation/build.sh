7030153ef30eabae3f9b46b386cf876dd10763d9
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-04 03:39:53 -0800

    minor tweaks k8s

--- deployments/text_generation/build.sh
+++ deployments/text_generation/build.sh
@@ -7,5 +7,5 @@ popd
 
 pushd .
 cd model
-# docker build -f Dockerfile.amd64 ../../../ -t kfastml.text_generation.model_server:v1
+docker build -f Dockerfile.amd64 ../../../ -t kfastml.text_generation.model_server:v1
 popd
