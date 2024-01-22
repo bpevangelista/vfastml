b92f47455154a3c1d22bf2748a6b16aefa4d3b13
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-21 23:50:37 -0800

    Fixed KV_cache and sdpa_attention mask

--- vfastml/models/model_gpu_partitioned.py
+++ vfastml/models/model_gpu_partitioned.py
@@ -2,8 +2,10 @@ from typing import Literal
 
 import torch
 import torch.nn as nn
-from transformers import DynamicCache, LogitsProcessorList, StoppingCriteriaList, PreTrainedModel, MaxLengthCriteria
-from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask
+from transformers import DynamicCache, LogitsProcessorList, StoppingCriteriaList, PreTrainedModel
+from transformers.generation import MaxLengthCriteria, MaxTimeCriteria
+from transformers.modeling_attn_mask_utils import _prepare_4d_causal_attention_mask_for_sdpa, \
+    _prepare_4d_causal_attention_mask
 
 
 class ModelHeaderPartition(nn.Module):
@@ -35,7 +37,7 @@ class MistralModelSingleGpuPartitioned(nn.Module):
                  header: ModelHeaderPartition,
                  blocks: list[ModelLayersPartition],
                  device: str,
-                 attn_implementation: Literal['sdpa', 'flash_attention_2', 'manual']
+                 attn_implementation: Literal['sdpa', 'flash_attention_2', 'manual'],
                  ):
         super(MistralModelSingleGpuPartitioned, self).__init__()
         self.header = header
@@ -43,9 +45,29 @@ class MistralModelSingleGpuPartitioned(nn.Module):
         self.device = device
         self.attn_implementation = attn_implementation
 
+
+    def _prepare_inputs(
+            self,
+            input_ids: torch.Tensor,
+            attention_mask_2d: torch.Tensor,
+            kv_cache: DynamicCache) -> tuple[torch.Tensor, torch.Tensor]:
+        position_ids = attention_mask_2d.long().cumsum(-1) - 1
+        position_ids.masked_fill_(attention_mask_2d == 0, 1)
+
+        # First run only
+        if kv_cache is not None:
+            past_length = kv_cache.seen_tokens
+            input_ids = input_ids[:, past_length:]
+            position_ids = position_ids[:, past_length:]
+
+        return input_ids, position_ids
+
+
     def forward(self,
                 input_ids: torch.Tensor,
+                attention_mask: torch.Tensor,
                 ):
+
         past_kv_cache = DynamicCache()
         logits_processor = LogitsProcessorList()
 
@@ -60,32 +82,44 @@ class MistralModelSingleGpuPartitioned(nn.Module):
         eos_token_id_tensor = torch.tensor([eos_token_id]).to(self.device)
 
         batch_count, seq_length = input_ids.shape
+        attention_mask_2d = attention_mask
         unfinished_batches = torch.ones(batch_count, dtype=torch.long, device=self.device)
 
-        is_done = False
-        while not is_done:
-            batch_count, seq_length = input_ids.shape
-            hidden_states = self.header.vocab_embedding(input_ids)
+        has_finished = False
+        while not has_finished:
+            unseen_input_ids, unseen_position_ids = self._prepare_inputs(input_ids, attention_mask_2d, past_kv_cache)
+            batch_count, seq_length = unseen_input_ids.shape
+            hidden_states = self.header.vocab_embedding(unseen_input_ids)
 
-            past_kv_length = past_kv_cache.get_usable_length(seq_length)
-            position_ids = torch.arange(past_kv_length,
-                                        seq_length + past_kv_length,
-                                        dtype=torch.long,
-                                        device=self.device).unsqueeze(0)
+            attention_mask = attention_mask_2d
+            if self.attn_implementation == 'flash_attention_2':
+                attention_mask = attention_mask if (0 in attention_mask) else None
+            else:
+                if self.attn_implementation == 'sdpa':
+                    mask_prepare_func = _prepare_4d_causal_attention_mask_for_sdpa
+                else:
+                    mask_prepare_func =_prepare_4d_causal_attention_mask
 
-            attention_mask = _prepare_4d_causal_attention_mask(
-                attention_mask=None,
-                input_shape=(batch_count, seq_length),
-                inputs_embeds=hidden_states,
-                past_key_values_length=past_kv_length,
-            )
+                attention_mask = mask_prepare_func(
+                    attention_mask=attention_mask,
+                    input_shape=(batch_count, seq_length),
+                    inputs_embeds=hidden_states,
+                    past_key_values_length=past_kv_cache.get_usable_length(seq_length),
+                )
 
-            for block in self.blocks: # per partition
+            block_count = len(self.blocks)
+            gpu_blocks = [self.blocks[0]]
+            for i in range(block_count):
+
+                block = gpu_blocks.pop()
+                next_block_idx = (i + 1) % block_count
+                gpu_blocks.append(self.blocks[next_block_idx])
+
                 for layer in block.layers:
                     outputs = layer(
                         hidden_states,
                         attention_mask=attention_mask,
-                        position_ids=position_ids,
+                        position_ids=unseen_position_ids,
                         past_key_value=past_kv_cache,
                         output_attentions=False,
                         use_cache=True,
@@ -94,23 +128,27 @@ class MistralModelSingleGpuPartitioned(nn.Module):
                     next_decoder_cache = outputs[1]
 
             past_kv_cache = next_decoder_cache
-
             hidden_states = self.header.lm_norm(hidden_states)
             all_token_logits = self.header.lm_head(hidden_states).float()
 
             next_token_logits = all_token_logits[:, -1, :]
             next_tokens_scores = logits_processor(input_ids, next_token_logits)
 
+            # One new token per-batch
             next_tokens = torch.argmax(next_tokens_scores, dim=-1)
             next_tokens = next_tokens * unfinished_batches + eos_token_id * (1 - unfinished_batches)
+            # TODO Stream Out next_tokens.cpu()
+
             input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
+            next_attn_mask = attention_mask_2d.new_ones((attention_mask_2d.shape[0], 1))
+            attention_mask_2d = torch.cat([attention_mask_2d, next_attn_mask], dim=-1)
 
             unfinished_batches = unfinished_batches.mul(
                 next_tokens.tile(eos_token_id_tensor.shape[0], 1).ne(eos_token_id_tensor.unsqueeze(1)).prod(dim=0)
             )
 
-            if unfinished_batches.max() == 0 or stopping_criteria(input_ids, ()):
-                is_done = True
+            if unfinished_batches.max() == 0 or stopping_criteria(input_ids, scores=None):
+                has_finished = True
 
         return input_ids
 
