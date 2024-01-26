3cd70b46e2da6122ce1e7af7e73034166c0e0d90
Bruno Evangelista <bpevangelista@gmail.com>
2024-01-26 09:04:57 -0800

    Update image server to match text server

--- vfastml/models_servers/model_server_image.py
+++ vfastml/models_servers/model_server_image.py
@@ -6,10 +6,10 @@ from vfastml.models_servers.model_server import ModelServer
 
 class ImageToImageModelServer(ModelServer, ABC):
     async def _rpc_step(self, rpc_obj: ImageToImageReq) -> dict:
-        result = await self._image_to_image(rpc_obj.request_id, rpc_obj.images, **(rpc_obj.extra_params or {}))
+        result = await self._image_to_image(rpc_obj.request_id, rpc_obj.images, rpc_obj.forward_params)
         assert isinstance(result, dict), "_image_to_image must return a dict"
         return result
 
     @abstractmethod
-    async def _image_to_image(self, request_id: str, images: list[str | bytes], **kwargs) -> dict:
+    async def _image_to_image(self, request_id: str, images: list[str | bytes], forward_params: dict) -> dict:
         raise NotImplementedError
